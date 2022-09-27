a0d6fa08838cf3fdbcee9e51b8f6536bb4b7b521
Cubolink <joaquin.cruz.c@ug.uchile.cl>
2022-09-27 20:52:37 -0300

    add: initial point sampling with fps

---
+++ fps.py
@@ -0,0 +1,72 @@
+# https://github.com/ziruiw-dev/farthest-point-sampling
+
+import os
+import openmesh
+import numpy as np
+from progress_bar import print_progress_bar
+
+
+class FPS:
+    def __init__(self, pcd_xyz, n_samples):
+        self.n_samples = n_samples
+        # self.pcd_xyz = pcd_xyz
+        self.n_pts = pcd_xyz.shape[0]
+        self.dim = pcd_xyz.shape[1]
+        self.selected_pts = None
+        self.selected_pts_indices = np.zeros(self.n_samples, dtype=int)
+        self.selected_pts_expanded = np.zeros(shape=(n_samples, 1, self.dim))
+        self.remaining_pts = np.copy(pcd_xyz)
+
+        self.dist_pts_to_selected = None  # Iteratively updated in step(). Finally re-used in group()
+
+        # Random pick a start
+        self.start_idx = np.random.randint(low=0, high=self.n_pts - 1)
+        self.selected_pts_expanded[0] = self.remaining_pts[self.start_idx]
+        self.selected_pts_indices[0] = self.start_idx
+        self.n_selected_pts = 1
+
+    def get_selected_pts(self):
+        self.selected_pts = np.squeeze(self.selected_pts_expanded, axis=1)
+        return self.selected_pts, self.selected_pts_indices
+
+    def _step(self):
+        if self.n_selected_pts < self.n_samples:
+            self.dist_pts_to_selected = self.__distance__(self.remaining_pts,
+                                                          self.selected_pts_expanded[:self.n_selected_pts]).T
+            dist_pts_to_selected_min = np.min(self.dist_pts_to_selected, axis=1, keepdims=True)
+            res_selected_idx = np.argmax(dist_pts_to_selected_min)
+
+            self.selected_pts_indices[self.n_selected_pts] = int(res_selected_idx)
+            self.selected_pts_expanded[self.n_selected_pts] = self.remaining_pts[res_selected_idx]
+            self.n_selected_pts += 1
+        else:
+            print("Got enough number samples")
+
+    def fit(self):
+        for _ in range(1, self.n_samples):
+            self._step()
+            print_progress_bar(_, self.n_samples-1, prefix='Progress:', length=20)
+        return self.get_selected_pts()
+
+    @staticmethod
+    def __distance__(a, b):
+        return np.linalg.norm(a - b, ord=2, axis=2)
+
+
+def compute_fps(filename, n_samples: int, pc=None):
+    name = os.path.splitext(filename)[0]
+    if os.path.exists(str(name) + "fp_sampled" + str(n_samples) + '.npz'):
+        data = np.load(str(name) + "fp_sampled" + str(n_samples) + '.npz')
+        return data['points'], data['indices']
+
+    if pc is None:
+        pc = openmesh.read_trimesh(filename).points()
+    sample_fps = FPS(pc, n_samples)
+    print("Initialised FPS sampler successfully.")
+    print("Running FPS over {0:d} points and geting {1:d} samples.".format(pc.shape[0],
+                                                                           n_samples))
+    sample, sample_idx = sample_fps.fit()  # Get all samples.
+    print("FPS sampling finished.")
+    np.savez_compressed(str(name) + "fp_sampled" + str(n_samples) + '.npz', points=sample, indices=sample_idx)
+
+    return sample, sample_idx
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import argparse
 import os
 import numpy as np
+from sklearn import neighbors
 
 import polyscope as ps
 import trimesh
 import laplace
 import openmesh
 from signature import compute_signature
+from fps import compute_fps
 
 
 if __name__ == '__main__':
@@ -25,10 +27,17 @@ if __name__ == '__main__':
     signature_extractor = compute_signature(args.file, args)
     hks = signature_extractor.heat_signatures(10)
 
+    # FPS
+    print("FPSampling")
+    point_cloud = mesh.points()
+    sample_points, sample_indices = compute_fps(args.file,
+                                                min(point_cloud.shape[0], max(point_cloud.shape[0]//100, 100)),
+                                                pc=point_cloud)
+
     ps.init()
 
     ps_mesh = ps.register_surface_mesh("mesh", mesh.points(), mesh.face_vertex_indices())
     ps_mesh.add_scalar_quantity("HKS (02nd descriptor)", hks[:, 1], cmap='coolwarm')
+    ps_cloud = ps.register_point_cloud("sample points", sample_points)
 
     ps.show()
-
