14b4a01ed6ec690492e02616a3643c16af4da55b
Cubolink <joaquin.cruz.c@ug.uchile.cl>
2022-09-29 00:47:23 -0300

    add: plots the supporting circles

--- main.py
+++ main.py
@@ -56,21 +56,29 @@ if __name__ == '__main__':
     print("FPSampling")
     point_cloud = mesh.points()
     sample_points, sample_indices = compute_fps(args.file,
-                                                min(point_cloud.shape[0], max(point_cloud.shape[0]//100, 100)),
+                                                min(point_cloud.shape[0], max(point_cloud.shape[0]//200, 30)),
                                                 pc=point_cloud)
 
     # knn
     print("(KNN) Finding Similar HKS points to the sampled points")
-    nbrs = neighbors.NearestNeighbors(n_neighbors=100, algorithm='ball_tree').fit(hks)
+    nbrs = neighbors.NearestNeighbors(n_neighbors=20, algorithm='ball_tree').fit(hks)
     nbrs_distances, nbrs_indices = nbrs.kneighbors(hks[sample_indices])
     print("Computing Supporting Circles")
-    compute_supporting_circles(point_cloud[nbrs_indices])
-
+    s_circles = compute_supporting_circles(point_cloud[nbrs_indices])
     ps.init()
 
     ps_mesh = ps.register_surface_mesh("mesh", mesh.points(), mesh.face_vertex_indices())
     ps_mesh.add_scalar_quantity("HKS (02nd descriptor)", hks[:, 1], cmap='coolwarm')
+
     ps_cloud = ps.register_point_cloud("sample points", sample_points)
-    ps_similar = ps.register_point_cloud("similar hks points", point_cloud[nbrs_indices[30]])
+    ps_similar = ps.register_point_cloud("similar hks points", point_cloud[nbrs_indices[10]])
+
+    circle_centers = np.array([s_circle[0][0] for s_circle in s_circles])
+    ps_circle_centers = ps.register_point_cloud("Supporting Circle Centers", circle_centers)
+
+    for i in range(len(s_circles)):
+        circle_nodes, circle_edges = generate_circle_node_edges(s_circles[i][0])
+        ps.register_curve_network(f"Supporting Circle {i+1:03d}, votes:{s_circles[i][1]}", circle_nodes, circle_edges, radius=0.001)
+        ps_circle_centers.add_vector_quantity("Normal", np.array([s_circle[0][2] for s_circle in s_circles]))
 
     ps.show()
