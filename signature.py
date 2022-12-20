07ea3873d08370efa304c6085ada254c90182f8b
Cubolink <joaquin.cruz.c@ug.uchile.cl>
2022-12-19 22:16:17 -0300

    CHG: Use robust_laplacian
    Solves problems with non-manifold objects, like those ones with missing geometry that caused the script to raise an error.

--- signature.py
+++ signature.py
@@ -1,6 +1,7 @@
 # https://github.com/ivansipiran/CC5513_Procesamiento_Geometrico
 
 import numpy as np
+import robust_laplacian
 import scipy
 import trimesh
 import logging
@@ -29,7 +30,8 @@ class SignatureExtractor(object):
             approx (str, optional): Laplace operator approximation to use. 
                                     Must be in ['beltrami', 'cotangens', 'mesh', 'fem']. Defaults to 'cotangens'.
         """
-        self.W, self.M = laplace.get_laplace_operator_approximation(mesh, approx)
+        # self.W, self.M = laplace.get_laplace_operator_approximation(mesh, approx)
+        self.W, self.M = robust_laplacian.mesh_laplacian(np.array(mesh.vertices), np.array(mesh.faces))
         self.n_basis = min(len(mesh.vertices) - 1, n)
 
         sigma = -0.01
